019f29d64b1b82db4b078a93552fe911f6f217a4
Joris Dewaele <joris.dewaele@tutanota.com>
2019-09-28 22:35:39 +0200

    ES6 Tooling setup

---
+++ src/greet.js
@@ -0,0 +1,5 @@
+function greet(firstName) {
+  return `Hello ${firstName}`;
+}
+
+export default greet;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import greet from './greet';
+
+const name = "Romain";
+
+console.log(greet(name));
